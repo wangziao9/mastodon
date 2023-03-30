666a9a85a430acbd95866072b819970a7f406863
Tdxdxoz <tdxdxoz@gmail.com>
2023-03-30 16:49:52 +0800

    default enable auto play gif

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -34,7 +34,7 @@ class UserSettings
     setting :reduce_motion, default: false
     setting :expand_content_warnings, default: false
     setting :display_media, default: 'default', in: %w(default show_all hide_all)
-    setting :auto_play, default: false
+    setting :auto_play, default: true
     setting :use_system_emoji_font, default: false
   end
 
